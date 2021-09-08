14afa4199531422f79fcb1944eb36bf8e34c99ee
dillin92 <dillin970@gmail.com>
2021-09-08 19:07:16 -0400

    built with React

---
+++ whatcha-eatin/src/components/searchBar/SearchBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+
+// const apiKey = "AIzaSyAoZvL9zENAx7_uSggjaC57K4JKzBR_9DY";
+
+// function getLocation() {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//       x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+//   };
+
+// function showPosition(position) {
+//     let location = position.coords.latitude + "," + position.coords.longitude;
+  
+//     let img_url = `"https://maps.googleapis.com/maps/api/staticmap?center=
+//     "+latlon+"&zoom=14&size=400x300&sensor=false&key=${apiKey}"`;
+  
+//     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+//   };
+
+// const connectApi = () => {
+
+   
+//     getLocation();
+//     showPosition();
+
+
+//     fetch(`'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=' + ${keyword} + '&location=${location}' + '&radius= ${radius}' + &type=restaurant&key=${apiKey}'`)
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+// };
+
+
+const SearchBar = () => {
+  return(
+<section>
+    <h1>Enter Your Search Here</h1>
+    <button>Submit</button>
+</section>
+  )
+}
+
+export default SearchBar;
