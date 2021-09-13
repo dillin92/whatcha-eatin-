da9f13b9bee7054c0aff6485d1ff86f8f3eda73d
dillin92 <dillin970@gmail.com>
2021-09-12 21:16:41 -0400

    fetch request take 1

--- whatcha-eatin/src/components/searchBar/index.js
+++ whatcha-eatin/src/components/searchBar/index.js
@@ -1,39 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
-
 const apiKey = "AIzaSyAoZvL9zENAx7_uSggjaC57K4JKzBR_9DY";
+const keyword = document.querySelector('#keyword');
 
-  
-function getLocation() {
-  console.log('yo, diggity');
+const fetchData = () => {
+  return axios(`'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${keyword}&inputtype=textquery&key=${apiKey}'`)
+ .then((response) => console.log(response.data));
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-};
-
-getLocation();
-
-function showPosition(position) {
-
-  let latlon= position.coords.latitude + "," + position.coords.longitude;
-
-  let img_url = `"https://maps.googleapis.com/maps/api/staticmap?center=
-  "+latlon+"&zoom=14&size=400x300&sensor=false&key=${apiKey}"`;
-
-  document.getElementById("#mapholder").innerHTML = "<img src='"+img_url+"'>";
-
-  console.log(latlon);
-};
-
-
-
-
+}
 
 
 const SearchBar = () => {
+  return(
+ 
+ <form id="search-form">
+         <div>
+           <label htmlFor="keyword">keyword:</label>
+           <input type="text" id="keyword" name="keyword" />
+         </div>
+        
+         <button type="submit" onClick={fetchData}>Submit</button>
+ 
+         <div id="mapholder"></div>
+
+ </form>
+ )
+ };
+ 
+ export default SearchBar;
+  
+// radius div
+//  <div> 
+//  <label htmlFor="radius">radius</label>
+//  <input type="text" id ="radius" name="radius" />
+// </div>
+
+
+// function getLocation() {
+//   console.log('yo, diggity');
+
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     alert("Geolocation is not supported by this browser.");
+//   }
+// };
+
+// getLocation();
+
+// function showPosition(position) {
+
+//   let latlon= position.coords.latitude + "," + position.coords.longitude;
+
+  
+
+//   // // let img_url = `"https://maps.googleapis.com/maps/api/staticmap?center=
+//   // // "+latlon+"&zoom=14&size=400x300&sensor=false&key=${apiKey}"`;
+//   // document.getElementById("#mapholder").innerHTML = "<img src='"+img_url+"'>";
+  
+
+//   console.log(latlon);
+// };
+
+
+// function search(){
+  
 
 //   const keyword = document.querySelector("#keyword");
 //   // const latlon= showPosition();
@@ -45,10 +76,7 @@ const SearchBar = () => {
 // const [isLoaded, setIsLoaded] = useState(false);
 // const [items, setItems] = useState([]);
 
-//   function handleSubmit(e) {
-//     e.prevent.default();
-//     console.log('you clicked submit');
-//   }
+
 
 //   useEffect( () => {
 //     axios(`'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${keyword}&inputtype=textquery&key=${apiKey}'`)
@@ -63,23 +91,9 @@ const SearchBar = () => {
 //   finally(() => {
 //     setLoading(false);
 //   })
+// }
 
- return(
-
-<form id="search-form">
-        <div>
-          <label htmlFor="keyword">keyword</label>
-          <input type="text" id="keyword" name="keyboard" />
-        </div>
-        <div> 
-          <label htmlFor="radius">radius</label>
-          <input type="text" id ="radius" name="radius" />
-        </div>
-        <button type="submit">Submit</button>
-
-        <div id="mapholder"></div>
-</form>
-)
-};
-
-export default SearchBar;
+// function handleSubmit(e) {
+//   e.prevent.default();
+//   console.log('you clicked submit');
+// }
