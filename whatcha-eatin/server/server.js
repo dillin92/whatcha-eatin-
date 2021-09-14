2df5d36c586d8926f16d43c727f48cb7845d748c
dillin92 <dillin970@gmail.com>
2021-09-13 22:03:11 -0400

    plan b

--- whatcha-eatin/server/server.js
+++ whatcha-eatin/server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const db = require('./config/connection');
+const cors =  require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express;
 
 app.use(express.urlencoded({ extended: false }));
 app.use (express.json());
+app.use(function(res,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods','POST,GET,DELETE, PUT, OPTIONS');
+    next();
+})
+app.use(cors());
 
 db.once('open', () => {
     app.listen(PORT, () => {
--- whatcha-eatin/src/components/searchBar/index.js
+++ whatcha-eatin/src/components/searchBar/index.js
@@ -1,95 +1,159 @@
-import React from 'react';
-import fetchData from '../Fetch';
+import React, { useEffects, useState } from 'react';
+const apiKey = "AIzaSyAoZvL9zENAx7_uSggjaC57K4JKzBR_9DY";
+const keyword = document.querySelector('#keyword');
+const positionStackApi = 'c2a1ef285d8ddc4bc62cbf11b35816dd';
+
+const restaurants = ['85C Bakery Cafe',
+  'A&W Restaurants',
+  "Arby's",
+  "Auntie Anne's",
+  "Big Boy Restaurants",
+  "Blaze Pizza",
+  "Boston Market",
+  "Buffalo Wild Wings",
+  "Burger King",
+  "Carl's Jr",
+  "Chipotle Mexican Grill",
+  "Church's Chicken",
+  "Cicis",
+  "Cinnabon",
+  "Culver's",
+  "Dairy Queen",
+  "Domino's Pizza",
+  "Dunkin' Donuts",
+  "Five Guys",
+  "Gatti's Pizza",
+  "Godfather's Pizza",
+  "Golden Chick",
+  "Hardee's",
+  "In-N-Out Burger",
+  "Jack in the Box",
+  "Jersey Mike's Subs",
+  "Jet's Pizza",
+  "Jollibee",
+  "KFC",
+  "Little Caesars",
+  "Long John Silver's",
+  "Louisiana Famous Fried Chicken",
+  "Marco's Pizza",
+  "McDonald's",
+  "MOD Pizza",
+  "Orange Julius",
+  "Panda Express",
+  "Papa Gino's",
+  "Papa John's Pizza",
+  "Peter Piper Pizza",
+  "The Pizza Company",
+  "Pizza Hut",
+  "Pizza Inn",
+  "Pollo Campero",
+  "Pollo Tropical",
+  "Popeyes",
+  "Quiznos",
+  "Sbarro",
+  "Sonic Drive-In",
+  "Starbucks",
+  "Subway",
+  "Sweet Frog",
+  "Taco Bell",
+  "TCBY",
+  "Tim Hortons",
+  "TKK Fried Chicken",
+  "Wendy's",
+  "Whataburger",
+  "White Castle",
+  "Wingstop",
+  "WingStreet"
+];
+let randomRestaurants = restaurants[Math.floor(Math.random() * restaurants.length -1)];
 
 
+const Randomizer = () =>{
+ 
 
-const SearchBar = () => {
   return(
  
- <form id="search-form">
+    <form id="search-form" >
+   
+       <h1>Whatcha Eatin!</h1 >
+            <button type="submit">GO!!!</button>
+            <div>{randomRestaurants}</div>
+    </form>
+    )
+}
 
-    <h1>Search for Food Here!</h1>
+// const SearchBar = () => {
 
-         <div>
-           <label htmlFor="keyword">keyword:</label>
-           <input type="text" id="keyword" name="keyword" />
-         </div>
+//   const [formState, setFormState] = useState({ keyword: '' });
+
+//   const [errorMessage] =  useState('');
+//   let { keyword } = formState;
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (!errorMessage) {
+//       console.log('Submit Form', formState);
+//     }
+//   };
+
+//   const handleChange = (e) => {
+//     if (!errorMessage) {
+//       setFormState({ ...formState, [e.target.name]: e.target.value });
+//       console.log('Handle Form', formState);
+//     }
+//   };
+
+//   const componentDidMount = () => {
+//     const url=`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${keyword}&inputtype=textquery&key=${apiKey}`;
+//     const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+//     console.log("url", url);
+
+    
+//    fetch("https://api.positionstack.com/v1/forward?access_key=" + positionStackApi, {
+//      method: 'GET',
+//      mode: 'cors',
+//      headers: {
+//        "Content-Type": "application/json",
+//        "Content-Type": "application/x-www-form-urlencoded",
+//        "Accept": "application/json"
+//      }
+//    }).then(data => {
+//     const response = data.json();
+//     console.log("logging fetch response",response, data);
+//     })
+//     .then(res =>{
+//       console.log(res);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })  };
+
+//   return(
+ 
+//  <form id="search-form" defaultValue={keyword} onSubmit={handleSubmit}>
+
+//     <h1>Search for Food Here!</h1 >
+
+//          <div>
+//            <label htmlFor="keyword">keyword:</label>
+//            <input type="text" id="keyword" name="keyword" onBlur={handleChange} />
+//          </div>
         
-         <button type="submit" onClick={fetchData}>Submit</button>
+//          <button type="submit" onClick={componentDidMount}>Submit</button>
  
-         <div id="mapholder"></div>
+//          <div id="mapholder"></div>
 
- </form>
- )
- };
+//  </form>
+//  )
+
+
+//  };
  
- export default SearchBar;
-  
-// radius div
-//  <div> 
-//  <label htmlFor="radius">radius</label>
-//  <input type="text" id ="radius" name="radius" />
-// </div>
-
-
-// function getLocation() {
-//   console.log('yo, diggity');
-
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//     alert("Geolocation is not supported by this browser.");
-//   }
-// };
-
-// getLocation();
-
-// function showPosition(position) {
-
-//   let latlon= position.coords.latitude + "," + position.coords.longitude;
-
+ export default Randomizer;
   
 
-//   // // let img_url = `"https://maps.googleapis.com/maps/api/staticmap?center=
-//   // // "+latlon+"&zoom=14&size=400x300&sensor=false&key=${apiKey}"`;
-//   // document.getElementById("#mapholder").innerHTML = "<img src='"+img_url+"'>";
-  
-
-//   console.log(latlon);
-// };
-
-
-// function search(){
-  
-
-//   const keyword = document.querySelector("#keyword");
-//   // const latlon= showPosition();
-//   const radius = document.querySelector("#radius");
-
-// const [error, setError] = useState(null);
-// const [data, setData] = useState(null);
-// const [loading, setLoading] = useState(true);
-// const [isLoaded, setIsLoaded] = useState(false);
-// const [items, setItems] = useState([]);
-
-
-
-//   useEffect( () => {
-//     axios(`'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${keyword}&inputtype=textquery&key=${apiKey}'`)
-//   }).then(response => {
-//     setData(response.data)
-//   }).catch(error => {
-//     console.error(
-//       "error fetching data: ", error
-//     );
-//     setError(error);
-//   }).
-//   finally(() => {
-//     setLoading(false);
-//   })
-// }
-
-// function handleSubmit(e) {
-//   e.prevent.default();
-//   console.log('you clicked submit');
-// }
+// https://maps.googleapis.com/maps/api/place/findplacefromtext/json
+//   ?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry
+//   &input=Museum%20of%20Contemporary%20Art%20Australia
+//   &inputtype=textquery
+//   &key=AIzaSyAoZvL9zENAx7_uSggjaC57K4JKzBR_9DY
